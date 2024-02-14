97fd027fa296fb0d7170cc751fe0339e497d751c
EZ118 <79049368+EZ118@users.noreply.github.com>
2024-02-14 18:07:41 +0800

    添加修改请求头referer的代码

--- background.js
+++ background.js
@@ -2,5 +2,33 @@
 
 chrome.runtime.onInstalled.addListener(function () {
     console.log('Bili WebApp Extension Installed!');
-    chrome.tabs.create({ url: "https://ez118.github.io/biliweb/#installed"});
+    chrome.tabs.create({ url: "https://ez118.github.io/biliweb/#installed" });
+});
+
+chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [
+        {
+            "id": 1,
+            "priority": 1,
+            "action": {
+                "type": 'modifyHeaders',
+                "requestHeaders": [
+                    {
+                        "header": 'Referer',
+                        "operation": 'set',
+                        "value": 'https://www.bilibili.com/'
+                    },
+                    {
+                        "header": 'Origin',
+                        "operation": 'set',
+                        "value": 'https://www.bilibili.com/'
+                    }
+                ]
+            },
+            "condition": {
+                "urlFilter": 'https://*.bilibili.com/*',
+                "resourceTypes": ["xmlhttprequest"]
+            }
+        }
+    ]
 });
