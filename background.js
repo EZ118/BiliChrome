1503cbba0c10ce4f6c1344e2b64322b3d648721d
EZ118 <79049368+EZ118@users.noreply.github.com>
2024-03-27 19:40:41 +0800

    支持B站右键菜单在BiliChrome中打开，加入options扩展设置界面（未完成）

--- background.js
+++ background.js
@@ -32,3 +32,38 @@ chrome.declarativeNetRequest.updateDynamicRules({
         }
     ]
 });
+
+
+
+/* 右键菜单 */
+// chrome.contextMenus.create({
+//     title: 'BiliChrome',
+//     id: 'search',
+//     type: 'normal',
+//     contexts: ['all'],
+// });
+// chrome.contextMenus.create({
+//     title: '在 BiliChrome 中观看',
+//     // parentId: 'search',
+//     id: 'viewInExt',
+//     type: 'normal',
+//     contexts: ['all'],
+// });
+
+chrome.contextMenus.create({
+    title: '在 BiliChrome 中观看',
+    id: 'viewInExt',
+    type: 'normal',
+    contexts: ['all'],
+});
+
+chrome.contextMenus.onClicked.addListener(function (item, tab) {
+    var url = item.pageUrl + "?";
+    url = url.split("?")[0];
+    var purl = url.split("/");
+    if (item.menuItemId == "viewInExt" && purl[3] == "video" && purl[4]) {
+        var cmd = 'bvid_' + purl[4];
+        chrome.tabs.create({ url: chrome.runtime.getURL('home.html') + "#" + cmd });
+    }
+});
+
